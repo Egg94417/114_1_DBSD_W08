66781976da36620d96f819bd4ae6e8f20345c312
Egg94417 <dicky94417@gmail.com>
2025-10-30 11:50:51 +0800

    T4

---
+++ ch7-2-2 copy 2.php
@@ -0,0 +1,11 @@
+SID:C112181111
+<BR>
+NAME:黃翊華
+<p>
+<?php 
+$weekday = array( 1=>"Mon", "Tue", "Wed", "Thu",
+                   "Fri", "Sat", "Sun");
+// 顯示陣列元素
+for ( $i = 1; $i <= count($weekday); $i++ )
+   echo "$i : $weekday[$i] ";
+?>
