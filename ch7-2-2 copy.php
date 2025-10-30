77627c8143245d9fe61fc8184bdcdf57fdface7a
Egg94417 <dicky94417@gmail.com>
2025-10-30 10:59:13 +0800

    T2

---
+++ ch7-2-2 copy.php
@@ -0,0 +1,12 @@
+
+<?php 
+$arr["color"] = "紅色";  // 更改陣列元素值
+$arr["name"] = "黃翊華";
+$arr["shape"] = "圓形";
+$arr[] = 50;   // 沒有指定, 鍵值為0
+
+foreach($arr as $k => $v){
+   echo "$k~ $v~ <HR>";
+}
+echo"<BR>";
+print_r(value: $arr);
