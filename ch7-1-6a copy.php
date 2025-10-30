52b8bb6229de23ac28bcb0e95fe1fb3acfe3e291
Egg94417 <dicky94417@gmail.com>
2025-10-30 11:43:17 +0800

    T2

---
+++ ch7-1-6a copy.php
@@ -0,0 +1,17 @@
+SID:C112181111
+<BR>
+NAME:黃翊華
+<p>
+<?php
+define("ANIMALS", [
+    "Dog", 
+    "Cat",
+    "Monkey",
+    "Bird",
+]);
+echo ANIMALS[1];
+echo "<br/>";
+echo ANIMALS[2];
+echo "<br/>";
+?>
+
